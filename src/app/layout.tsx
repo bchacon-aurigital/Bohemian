88782e151d2666b5a9a85e2fc6deed10884fae97
bchacon-aurigital <bchacon@mainlydigitalworks.com>
2025-05-19 08:58:26 -0600

    GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "aos/dist/aos.css";
 import I18nProvider from "@/components/I18nProvider";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "The Bohemian Lagarto | Hotel Playa Lagarto en Costa Rica",
@@ -95,6 +96,20 @@ export default function RootLayout({
       </head>
       <body>
         <I18nProvider>{children}</I18nProvider>
+        
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-F79B9ETYTY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F79B9ETYTY');
+          `}
+        </Script>
       </body>
     </html>
   );
